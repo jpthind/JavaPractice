0693ccd144f4853f196ea5ab500c9c02d819684d
jpthind <pauljatinder@gmail.com>
2017-11-25 10:14:28 -0800

    Adding new files

---
+++ src/com/jp/practice/PassByValue.java
@@ -0,0 +1,21 @@
+package com.jp.practice;
+
+import static java.lang.System.out;
+
+public class PassByValue {
+
+    public static void main(String[] args) {
+        PassByValue p = new PassByValue();
+        int i = 10;
+        p.incrementInt(i);
+        out.println(i);
+
+    }
+
+    public void incrementInt(int i) {
+
+        i++;
+        out.println(i);
+    }
+
+}
---
+++ src/com/jp/practice/Singleton.java
@@ -0,0 +1,4 @@
+package com.jp.practice;
+
+public class Singleton {
+}
